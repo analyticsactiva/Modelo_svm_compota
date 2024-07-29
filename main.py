732d147cf523a17883dc225c90dda9ed6f688d31
analyticsactiva <analytics@activasite.com>
2024-07-29 09:42:29 -0400

    Update main.py
    Volver al csv, mientras se solucione la salida en excel

--- main.py
+++ main.py
@@ -387,10 +387,10 @@ if st.session_state["authentication_status"]:
                 #writer.close()
                 #book.save(uploaded_file)
                 #book.close()
-        output_file = to_excel(df_new)
+        #output_file = to_excel(df_new)
         #output_file = df_new.to_excel(index=False, sheet_name='Cluster_Pred', engine='openpyxl')
-        #output_file = df_new.to_csv(index=False).encode('utf-8')
-        st.download_button("Descargar", output_file,'Cluster.xlsx') #uploaded_file.name)
+        output_file = df_new.to_csv(index=False).encode('utf-8')
+        st.download_button("Descargar", output_file,'Cluster.csv') #uploaded_file.name)
     #st.write("")
     
     #with cols[3]:
